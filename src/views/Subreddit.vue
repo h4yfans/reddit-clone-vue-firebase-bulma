97221d3d0e12627333eb8bc87427a3b46848f251
Kaan <kaan94karaca@gmail.com>
2018-08-28 19:06:56 +0300

    - Hide submit form if not logged in - Show usernames - Show user images

--- src/views/Subreddit.vue
+++ src/views/Subreddit.vue
@@ -18,22 +18,20 @@
             <div class="card column is-4" v-for="post in posts" :key="post.id">
                 <div class="card-image" v-if="isImage(post.url)">
                     <figure class="image">
-                        <img :src="post.url" alt="Placeholder image">
+                        <img :src="post.url.image" alt="Placeholder image">
                     </figure>
                 </div>
                 <div class="card-content">
                     <div class="media">
                         <div class="media-left">
-                            <figure class="image is-48x48">
-                                <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
-                            </figure>
+                            <figure class="image is-48x48"><img :src="loadedUsersById[post.user_id].image" alt="Placeholder image"></figure>
                         </div>
                         <div class="media-content">
                             <p class="title is-4" v-if="!post.url">{{ post.title }}</p>
                             <p class="title is-4" v-if="post.url">
                                 <a :href="post.url" target="_blank"> {{ post.title }}</a>
                             </p>
-                            <p class="subtitle is-6">@johnsmith</p>
+                            <p class="subtitle is-6">{{ loadedUsersById[post.user_id].name }}</p>
                         </div>
                     </div>
 
@@ -61,10 +59,11 @@ export default {
     },
   }),
   mounted() {
+    this.initUser();
     this.initSubreddit(this.$route.params.name);
   },
   watch: {
-    '$route.params.name'() {
+    '$route.params.name': function () {
       this.initSubreddit(this.$route.params.name);
     },
     subreddit() {
@@ -75,13 +74,27 @@ export default {
   },
   computed: {
     ...mapState('subreddit', ['posts']),
-    ...mapGetters('subreddit', ['subreddit']),
+    ...mapGetters({
+      subreddit: 'subreddit/subreddit',
+      usersById: 'users/userById',
+    }),
+    loadedUsersById() {
+      return this.posts.reduce((byId, post) => {
+        byId[post.user_id] = this.usersById[post.user_id] || {
+          name: 'Loading...',
+          image: 'https://bulma.io/images/placeholders/48x48.png',
+        };
+
+        return byId;
+      }, {});
+    },
   },
   methods: {
     isImage(url) {
       return url.match(/(png|jpg|jpeg|gif|svg)$/);
     },
     ...mapActions('subreddit', ['createPost', 'initSubreddit', 'initPosts']),
+    ...mapActions('users', { initUser: 'init' }),
     async onCreatePost() {
       if (this.post.title && (this.post.description || this.post.URL)) {
         this.createPost(this.post);
@@ -93,12 +106,42 @@ export default {
         this.showForm = false;
       }
     },
+    getCreated(index) {
+      function timeSince(date) {
+        const seconds = Math.floor((new Date() - date) / 1000);
+        let interval = Math.floor(seconds / 31536000);
+        if (interval > 1) {
+          return `${interval} years`;
+        }
+        interval = Math.floor(seconds / 2592000);
+        if (interval > 1) {
+          return `${interval} months`;
+        }
+        interval = Math.floor(seconds / 86400);
+        if (interval > 1) {
+          return `${interval} days`;
+        }
+        interval = Math.floor(seconds / 3600);
+        if (interval > 1) {
+          return `${interval} hours`;
+        }
+        interval = Math.floor(seconds / 60);
+        if (interval > 1) {
+          return `${interval} minutes`;
+        }
+        return `${Math.floor(seconds)} seconds`;
+      }
+
+      return timeSince(this.posts[index].created_at.seconds * 1000) < 0 ?
+        '0 seconds ago' :
+        `${timeSince(this.posts[index].created_at.seconds * 1000)} ago`;
+    },
   },
 };
 </script>
 
 <style scoped>
-.posts{
-    margin-top: 2em;
-}
+    .posts {
+        margin-top: 2em;
+    }
 </style>
